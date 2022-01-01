2f7b63c343f366ed2634ebc12c8d148c58d4764d
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2022-01-01 08:34:20 -0800

    build: init react app

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <section>
+        <h2>Hello World!</h2>
+      </section>
+    </div>
+  )
+}
